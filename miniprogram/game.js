d415d62d5aad5a14e3727c7f206074575bef71d9
wusideng <guohd1985@hotmail.com>
2021-07-02 14:54:35 +0800

    add  minigame collection

---
+++ miniprogram/game.js
@@ -0,0 +1,28 @@
+
+
+/**
+ * 光线效果
+ * webgl 使用
+ * web-aqua
+ */
+// import "./webgl-aqua/game.js"
+
+/**
+ * 粒子效果
+ * webgl 使用
+ * webgl-compute
+ * WebGL Compute，Canvas Compute 的 WebGL 版本，用 WebGL 绘制点取代 Canvas 绘制短线段；
+ */
+// import "./webgl-compute/game.js"
+
+/**
+ * 飞机小游戏
+ */
+// import "./canvas-bitmap/game.js"
+
+/**
+ * 2D 粒子效果
+ */
+// import "./canvas-compute/game.js"
+
+import "./canvas-fishie/game.js"
---
+++ miniprogram/index-demo.js
@@ -0,0 +1,49 @@
+
+const canvas = wx.createCanvas()
+const context = canvas.getContext('2d')
+context.fillStyle = '#1aad19'
+context.fillRect(canvas.width / 2 - 50, 0, 100, 100)
+
+const image = wx.createImage()
+let imgX = canvas.width / 2 - 75
+let imgY = 500
+
+image.onload = function () { }
+
+image.src = 'images/hero.png'
+
+wx.onTouchMove(res => {
+  imgX = res.changedTouches[0].clientX - 75
+  imgY = res.changedTouches[0].clientY
+})
+
+const { windowWidth, windowHeight } = wx.getSystemInfoSync()
+const rectX = canvas.width / 2 - 50
+let rectY = 0
+var intervHero1 = setInterval(function () {
+  if (distoryImg()) {
+    crash(intervHero1)
+  }
+  ++rectY
+  drawRect(rectX, rectY++)
+  context.drawImage(image, imgX, imgY)
+}, 16)
+
+function drawRect(x, y) {
+  context.clearRect(0, 0, windowWidth, windowHeight)
+  context.fillRect(x, y, 100, 100)
+}
+function distoryImg() {
+  // if (imgX >= rectX - 100 && imgX < rectX + 100 && imgY >= rectY - 100 && imgY <= rectY + 100) {
+  if (imgX >= rectX - 100 && imgX < rectX + 100 && imgY === rectY + 100) {
+    console.log("rectX:", rectX, "rectY:", rectY, "imgY:", imgY)
+    return true
+  } else {
+    return false
+  }
+}
+function crash(intervalId) {
+  // clearInterval(intervalId)
+  console.log("crash!!!")
+  image.src = 'images/explosion19.png'
+}
